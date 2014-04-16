68ce6febc123dda691b2918ff438372bdf8182d7
Steven Haryanto (on PC, Jakarta) <stevenharyanto@gmail.com>
2014-04-16 19:31:16 +0700

    Initial commit, split from Perinci-Util dist

---
+++ lib/Perinci/Sub/Util.pm
@@ -0,0 +1,188 @@
+package Perinci::Sub::Util;
+
+use 5.010001;
+use strict;
+use warnings;
+
+use Scalar::Util qw (looks_like_number);
+
+require Exporter;
+our @ISA = qw(Exporter);
+our @EXPORT_OK = qw(
+                       err
+                       caller
+               );
+
+# VERSION
+
+our $STACK_TRACE;
+
+our @_c; # to store temporary celler() result
+our $_i; # temporary variable
+
+sub err {
+
+    # get information about caller
+    my @caller = CORE::caller(1);
+    if (!@caller) {
+        # probably called from command-line (-e)
+        @caller = ("main", "-e", 1, "program");
+    }
+
+    my ($status, $msg, $meta, $prev);
+
+    for (@_) {
+        my $ref = ref($_);
+        if ($ref eq 'ARRAY') { $prev = $_ }
+        elsif ($ref eq 'HASH') { $meta = $_ }
+        elsif (!$ref) {
+            if (looks_like_number($_)) {
+                $status = $_;
+            } else {
+                $msg = $_;
+            }
+        }
+    }
+
+    $status //= 500;
+    $msg  //= "$caller[3] failed";
+    $meta //= {};
+    $meta->{prev} //= $prev if $prev;
+
+    # put information on who produced this error and where/when
+    if (!$meta->{logs}) {
+
+        # should we produce a stack trace?
+        my $stack_trace;
+        {
+            no warnings;
+            # we use Carp::Always as a sign that user wants stack traces
+            last unless $STACK_TRACE // $INC{"Carp/Always.pm"};
+            # stack trace is already there in previous result's log
+            last if $prev && ref($prev->[3]) eq 'HASH' &&
+                ref($prev->[3]{logs}) eq 'ARRAY' &&
+                    ref($prev->[3]{logs}[0]) eq 'HASH' &&
+                        $prev->[3]{logs}[0]{stack_trace};
+            $stack_trace = [];
+            $_i = 1;
+            while (1) {
+                {
+                    package DB;
+                    @_c = CORE::caller($_i);
+                    if (@_c) {
+                        $_c[4] = [@DB::args];
+                    }
+                }
+                last unless @_c;
+                push @$stack_trace, [@_c];
+                $_i++;
+            }
+        }
+        push @{ $meta->{logs} }, {
+            type    => 'create',
+            time    => time(),
+            package => $caller[0],
+            file    => $caller[1],
+            line    => $caller[2],
+            func    => $caller[3],
+            ( stack_trace => $stack_trace ) x !!$stack_trace,
+        };
+    }
+
+    #die;
+    [$status, $msg, undef, $meta];
+}
+
+sub caller {
+    my $n0 = shift;
+    my $n  = $n0 // 0;
+
+    my $pkg = $Perinci::Sub::Wrapper::default_wrapped_package //
+        'Perinci::Sub::Wrapped';
+
+    my @r;
+    my $i =  0;
+    my $j = -1;
+    while ($i <= $n+1) { # +1 for this sub itself
+        $j++;
+        @r = CORE::caller($j);
+        last unless @r;
+        if ($r[0] eq $pkg && $r[1] =~ /^\(eval /) {
+            next;
+        }
+        $i++;
+    }
+
+    return unless @r;
+    return defined($n0) ? @r : $r[0];
+}
+
+1;
+# ABSTRACT: Helper when writing functions
+
+=head1 SYNOPSIS
+
+ use Perinci::Sub::Util qw(err caller);
+
+ sub foo {
+     my %args = @_;
+     my $res;
+
+     my $caller = caller();
+
+     $res = bar(...);
+     return err($err, 500, "Can't foo") if $res->[0] != 200;
+
+     [200, "OK"];
+ }
+
+
+=head1 FUNCTIONS
+
+=head2 caller([ $n ])
+
+Just like Perl's builtin caller(), except that this one will ignore wrapper code
+in the call stack. You should use this if your code is potentially wrapped. See
+L<Perinci::Sub::Wrapper> for more details.
+
+=head2 err(...) => ARRAY
+
+Experimental.
+
+Generate an enveloped error response (see L<Rinci::function>). Can accept
+arguments in an unordered fashion, by utilizing the fact that status codes are
+always integers, messages are strings, result metadata are hashes, and previous
+error responses are arrays. Error responses also seldom contain actual result.
+Status code defaults to 500, status message will default to "FUNC failed". This
+function will also fill the information in the C<logs> result metadata.
+
+Examples:
+
+ err();    # => [500, "FUNC failed", undef, {...}];
+ err(404); # => [404, "FUNC failed", undef, {...}];
+ err(404, "Not found"); # => [404, "Not found", ...]
+ err("Not found", 404); # => [404, "Not found", ...]; # order doesn't matter
+ err([404, "Prev error"]); # => [500, "FUNC failed", undef,
+                           #     {logs=>[...], prev=>[404, "Prev error"]}]
+
+Will put C<stack_trace> in logs only if C<Carp::Always> module is loaded.
+
+
+=head1 FAQ
+
+=head2 What if I want to put result ($res->[2]) into my result with err()?
+
+You can do something like this:
+
+ my $err = err(...) if ERROR_CONDITION;
+ $err->[2] = SOME_RESULT;
+ return $err;
+
+
+=head1 SEE ALSO
+
+L<Perinci::Util>
+
+L<Perinci>
+
+=cut
