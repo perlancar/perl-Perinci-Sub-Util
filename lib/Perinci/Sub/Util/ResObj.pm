82cfb64e25743c85d9bd8258dfe75ac0453d3f5b
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2023-07-02 17:08:37 +0700

    [bugfix] Fix typo in variable name

--- lib/Perinci/Sub/Util/ResObj.pm
+++ lib/Perinci/Sub/Util/ResObj.pm
@@ -1,13 +1,17 @@
 package Perinci::Sub::Util::ResObj;
 
-# DATE
-# VERSION
-
+use strict;
 use Carp;
+
 use overload
     q("") => sub {
-        my $res = shift; "ERROR $err->[0]: $err->[1]\n" . Carp::longmess();
+        my $res = shift; "ERROR $res->[0]: $res->[1]\n" . Carp::longmess();
     };
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
 
 1;
 # ABSTRACT: An object that represents enveloped response suitable for die()-ing
@@ -16,4 +20,3 @@ use overload
 
 Currently unused. See L<Perinci::Sub::Util>'s C<warn_err> and C<die_err>
 instead.
-
